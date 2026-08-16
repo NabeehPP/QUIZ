14a03c8f114976d3fac3667876843374ed621032
nabeeh <nabeeh3528@gmail.com>
2026-08-16 14:36:23 +0530

    new

--- components/SiteTrademark.tsx
+++ components/SiteTrademark.tsx
@@ -2,8 +2,8 @@
 
 export default function SiteTrademark() {
   return (
-    <div className="fixed bottom-3 left-1/2 -translate-x-1/2 z-[100] pointer-events-none">
-      <div className="rounded-lg border border-white/10 bg-white/[0.03] backdrop-blur-md px-3 py-1 shadow-sm">
+    <div className="fixed top-3 left-1/2 -translate-x-1/2 z-[100] pointer-events-none">
+      <div className="rounded-lg border border-white/10 bg-white/[0.03] backdrop-blur-md px-3 py-0.5 shadow-sm">
         <span className="text-[9px] font-medium tracking-[0.15em] uppercase text-white/40">
           Created by{" "}
           <span className="text-white/60">Nabeeh</span>
